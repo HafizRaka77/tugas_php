79c7fb713033b700b98ef8b2556c389b96aee3d6
HafizRaka77 <hafizraka14@gmail.com>
2023-10-17 21:04:18 +0700

    Add new file

---
+++ sesi25/sesi25.php
@@ -0,0 +1,32 @@
+<?php
+    //no 1 Membuat output bilangan ganjil dan genap
+    echo "Tugas no 1 : <br>";
+    $bil=10;
+        if ($bil % 2 == 0){ 
+            echo "$bil Merupakan Bilangan Genap <br><br>"; 
+        }else {
+            echo "$bil Merupakan Bilangan Ganjil <br><br>"; 
+    }
+    //no 2 Membuat tahun kabisat
+    echo "Tugas no 2 : <br>";
+    $tahun = 2023;
+        if ($tahun % 4 == 0){ 
+            echo "$tahun Merupakan tahun kabisat <br><br>"; 
+        }else {
+            echo "$tahun Bukan merupakan tahun kabisat <br><br>"; 
+    }
+    //no 3 Membuat grade nilai
+    echo "Tugas no 3 : <br>";
+    $nilai = 95;
+        if ($nilai >= 90 && $nilai <= 100){ 
+            echo "$nilai Nilai A <br><br>"; 
+        }else if ($nilai >= 80 && $nilai <= 89) {
+            echo "$nilai Nilai B  <br><br> ";
+        }else if ($nilai >= 70 && $nilai <= 79) {
+            echo "$nilai Nilai C <br><br>";
+        }else if ($nilai >= 60 && $nilai <= 69) {
+            echo "$nilai Nilai D <br><br>";
+        }else if($nilai < 60) {
+            echo "$nilai Nilai E <br><br>";
+    }
+?>
